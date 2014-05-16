260b9c4fa32295b243c5a3d927718404c4e63fb0
cwbishop <chriswbishop@gmail.com>
2014-05-16 13:23:09 -0700

    SIN structure description (for adaptive play) and SIN_TestSetup (as replacement for SIN_defaults)

--- README.m
+++ README.m
@@ -1,22 +1,21 @@
 =======================================================================================================
-Options -> What the user has to provide to get the player functions to work. Options structures can be tailored to each player. The example below configures the portaudio_adaptiveplay function to administer the HINT. 
+Test Setup -> What the user has to provide to get the player functions to work. Options structures can be tailored to each player. The example below configures the portaudio_adaptiveplay function to administer the HINT. 
 
-	-> General settings: Generally static information across an experiment (e.g., subject ID motif, etc.)
+	-> General settings (general): Generally static information across an experiment (e.g., subject ID motif, etc.)
 		-> root: root directory for speech in noise (SIN) suite (required)		
 		
-	-> Test Parameters: parameters may vary, but some fields are required (e.g., testID). Example below is for the HINT. 
+	-> Test specific args (specific): parameters may vary, but some fields are required (e.g., testID). Example below is for the HINT. 
 		-> testID: string with description test ID. (required)
 		-> root: path to root directory where test specific materials are stored. 
 		-> subjectID_regexp: regular expression describing subject ID motif. 
-		-> list (rename to lookup_file): information about lookup list containing word information, list IDs, etc.
-			-> filename: full path to the file containing list information (used by importHINT)
-			-> sheetnum: For Excel spreadsheets, we need to know which sheet to load - here, we load sheet two (2) (used by importHINT)
-		-> list_filt: regular expression to use in finding available list (for HINT, this is done by looking up available directories following the regexp 'List[0-9]{2}') (used by SIN_GUI)
-						
+        -> list_filt (rename to list_regexp): regular expression to use in finding available list (for HINT, this is done by looking up available directories following the regexp 'List[0-9]{2}') (used by SIN_GUI)
+		-> hint_lookup: information about lookup list containing word information, list IDs, etc.
+            -> filename: full path to the file containing list information (used by importHINT)
+			-> sheetnum: For Excel spreadsheets, we need to know which sheet to load - here, we load sheet two (2) (used by importHINT)            
+				
+	-> Player settings (player): Options used by the designated player (example for portaudio_adaptiveplay).
 		
-	-> Player settings: Options used by the designated player (example for portaudio_adaptiveplay).
-		
-		-> player_handle: function handle to player (e.g., @portaudio_adaptiveplay) (required)
+		-> player_handle: function handle to player (e.g., @portaudio_adaptiveplay) (required) This might be used by SIN_RunTest to administer tests by ID)
 		
 		-> Playback (Playback Parameters)			
 			-> block_dur: buffer block duration in seconds
@@ -32,9 +31,9 @@ Options -> What the user has to provide to get the player functions to work. Opt
 		-> adaptive_mode:
 		-> append_files: 
 		-> stop_if_error:
-		-> looped_playback:
+		-> looped_playback (not implemented):
 		-> playback_channels:
-		-> randomize: randomize playback list. This is currently handled in SIN_runTest, but I think it would be smarter to move this centralize playback features like this to the "player" (e.g., portaudio_adaptiveplay)
+		-> randomize: (not implemented) randomize playback list. This is currently handled in SIN_runTest, but I think it would be smarter to move this centralize playback features like this to the "player" (e.g., portaudio_adaptiveplay)
 
 		(Buffer Windowing)
 		-> window_fhandle: windowing function handle
@@ -49,7 +48,8 @@ Options -> What the user has to provide to get the player functions to work. Opt
 		
 		(Modification Check: information will vary, example below for HINT_modcheck_GUI.m)
 		-> modcheck
-			-> playback_list: XXX this one could be a problem XXX
+            -> fhandle: function handle
+			-> playback_list: XXX this one could be a problem XXX XXX Not if we add the playback_list to the results structure and pass that around XXX
 			-> scoring_method: 
 			-> scoring_labels: 
 		
@@ -59,9 +59,10 @@ Options -> What the user has to provide to get the player functions to work. Opt
 			-> change_step: 
 			-> channels: 
 			-> scale_mode: 
+            
 =======================================================================================================
 Results Structure: Player return structure. This contains three basic fields
 	-> User Options (options provided by user, see Options structure above). This field can be used to relaunch the same test with the same settings (although playback order might change). 
-	-> RunTime: modified (and appended) options structure. This may contain additional fields not present in User Options. The fields will vary by player type. Example below for portaudio_adaptiveplay
+	-> RunTime: modified (and appended) options structure. This may contain additional fields not present in User Options. The fields will vary by player type. Example below for portaudio_adaptiveplay. Only additional top-level fields are desribed below.
 		-> playback_list: cell array of playback files
 		-> voice_recording: cell array of recorded responses if the player is configured to record subject responses through the recording device (see Record field above). (should be added at end of playback, I think, to keep structure size down)
--- SIN_TestSetup.m
+++ SIN_TestSetup.m
@@ -1,16 +1,113 @@
-opts=SIN_TestSetup(testID)
+function opts=SIN_TestSetup(testID)
 %
 % Should also spit back a testlist. This is needed for SIN_GUI
 opts=struct();
 
 switch testID
-    case 'default'
+    
+    case 'Project AD'
         
         % Set some default values, like device settings, that will be used
         % by multiple tests. 
+        
+        % Root SIN directory
+        opts.general.root='C:\Users\cwbishop\Documents\GitHub\SIN';
+        
+        % List of available tests
+        %   This will vary by project. Field used to generate test list in
+        %   SIN_GUI. CWB does not recall using it elsewhere. 
+        opts.general.testlist = {{'HINT (SNR-50)', 'PPT', 'ANL', 'Hagerman'}}; 
+        
+        % subject ID motif. Described using regexp. Used in
+        % SIN_register_subject.m
+        opts.specific.subjectID_regexp='^[1 2][0-9]{3}$';
+        
+        % Set sound output paramters. 
+        opts.player.playback = struct( ...
+            'device', portaudio_GetDevice(8), ... % device structure
+            'block_dur', 0.08, ... % 80 ms block duration.
+            'fs', 44100); % sampling rate            
+        
+        % Recording device
+        opts.player.record = struct( ...
+            'device', portaudio_GetDevice(6), ... % device structure
+            'buffer_dur', 30, ... recording buffer duration. Make this longer than you'll ever need for a single trial of HINT
+            'fs', 44100); % recording sampling rate
+        
+        % Stop playback if we encounter an error
+        opts.player.stop_if_error = true; 
+        
     case 'HINT (SNR-50)'
         
+        % ============================
+        % Get default information
+        % ============================
+        opts=SIN_TestSetup('Project AD');
+        
+        % ============================
+        % Test specific information. These arguments are used by
+        % test-specific auxiliary functions, like importHINT and the like. 
+        % ============================
+        
+        % set the testID (required)
+        opts.specific.testID='HINT (SNR-50)';
+        
+        % root directory for HINT stimuli and lookup list
+        opts.specific.root='C:\Users\cwbishop\Documents\GitHub\SIN\playback\HINT';
+        
+        % full path to HINT lookup list. Currently an XLSX file provided by
+        % Wu. Used by importHINT.m
+        opts.specific.hint_lookup=struct(...
+            'filename', 'C:\Users\cwbishop\Documents\GitHub\SIN\playback\HINT\HINT.xlsx', ...
+            'sheetnum', 2); 
+        
+        % ============================
+        % Player configuration
+        %   The fields below are used by the designated player to configure
+        %   playback.
+        %
+        %   - Note: some of these are set in 'Project AD' above
+        % ============================
+        
+        % Function handle for designated player
+        opts.player.player_handle = @portaudio_adaptiveplay; 
+        
+        opts.player = varargin2struct( ...
+            opts.player, ...
+            'adaptive_mode',    'byfile', ... % 'byfile' means modchecks performed after each trial.
+            'append_files',     false, ...  % append files before playback (makes one long trial)
+            'playback_channels',[1 2], ... % channels to present sounds to. 
+            'window_fhandle',   @hann, ...
+            'window_dur',       0.005, ... % window duration in seconds.
+            'unmod_playbackmode', 'stopafter', ... % stop unmodulated noise playback after each trial
+            'unmod_channels',   [1 2], ...
+            'unmod_leadtime',   1, ... % start unmodulated sound 1 sec before sentence onset
+            'unmod_lagtime',    1, ... % continue noise 1 sec after sentence ends
+            'unmod_playback',   {{fullfile(opts.specific.root, 'HINT-Noise.wav')}}); % noise file
+            
+        % ============================
+        % Modification check (modcheck) configuration        
+        % ============================
+        opts.player.modcheck=struct(...
+            'fhandle',         @HINT_modcheck_GUI, ...
+            'scoring_method',  'sentence_based', ... % score whole sentence (as traditionally done)
+            'scoring_labels',   {{'Correct', 'Incorrect'}}); % scoring labels for GUI
+        
+        % ============================
+        % Modifier configuration        
+        %   We use two modifiers, but CWB can't recall why. Need to check
+        %   this. 
+        % ============================
+        opts.player.modifier{1}=struct( ...
+            'fhandle',  @modifier_dBscale, ... % use a decibel scale
+            'dBstep',   [4 2], ...  % decibel step size (4 dB, then 2 dB)
+            'change_step', [1 5], ...   % trial on which to change the step size
+            'channels', 2, ...  % apply modification to channel 2
+            'scale_mode',   'cumulative');  % use cumulative scale mode. 
+            
     otherwise
+        
         error('unknown testID')
+        
 end % switch 
 
