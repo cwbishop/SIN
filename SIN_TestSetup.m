6f15b2bdd2214984335e35fd5c7612d7775e2295
cwbishop <chriswbishop@gmail.com>
2014-05-16 23:45:29 -0700

    Added ANL to SIN_TestSetup.m

--- SIN_TestSetup.m
+++ SIN_TestSetup.m
@@ -24,15 +24,14 @@ switch testID
         
         % Root SIN directory
         opts.general.root='C:\Users\cwbishop\Documents\GitHub\SIN';
+        % subject ID motif. Described using regexp. Used in
+        % SIN_register_subject.m
+        opts.general.subjectID_regexp='^[1 2][0-9]{3}$';
         
         % List of available tests
         %   This will vary by project. Field used to generate test list in
         %   SIN_GUI. CWB does not recall using it elsewhere. 
         opts.general.testlist = {{'HINT (SNR-50)', 'PPT', 'ANL', 'Hagerman'}}; 
-        
-        % subject ID motif. Described using regexp. Used in
-        % SIN_register_subject.m
-        opts.specific.subjectID_regexp='^[1 2][0-9]{3}$';
         
         % Set sound output paramters. 
         opts.player.playback = struct( ...
@@ -65,7 +64,7 @@ switch testID
         opts.specific.testID='HINT (SNR-50)';
         
         % root directory for HINT stimuli and lookup list
-        opts.specific.root='C:\Users\cwbishop\Documents\GitHub\SIN\playback\HINT';
+        opts.specific.root=fullfile(opts.general.root, 'playback', 'HINT');
         
         % full path to HINT lookup list. Currently an XLSX file provided by
         % Wu. Used by importHINT.m
@@ -117,8 +116,75 @@ switch testID
             'dBstep',   [4 2], ...  % decibel step size (4 dB, then 2 dB)
             'change_step', [1 5], ...   % trial on which to change the step size
             'channels', 2, ...  % apply modification to channel 2
-            'scale_mode',   'cumulative', ...% use cumulative scale mode. 
-            'plot', 1);  % plot results (to HINT_GUI)
+            'scale_mode',   'cumulative'); % use cumulative scale mode.             
+            
+    case 'ANL'
+        
+        % Test setup for the Acceptable Noise Level (ANL) Test
+        % ============================
+        % Get default information
+        % ============================
+        opts=SIN_TestSetup('Project AD');
+        
+        % ============================
+        % Test specific information. These arguments are used by
+        % test-specific auxiliary functions, like importHINT and the like. 
+        % ============================
+        
+        % set the testID (required)
+        opts.specific.testID='ANL';
+        
+        % root directory for HINT stimuli and lookup list
+        opts.specific.root=fullfile(opts.general.root, 'playback', 'ANL');
+              
+        % ============================
+        % Player configuration
+        %   The fields below are used by the designated player to configure
+        %   playback.
+        %
+        %   - Note: some of these are set in 'Project AD' above
+        % ============================
+        
+        % Function handle for designated player
+        opts.player.player_handle = @portaudio_adaptiveplay; 
+        
+        opts.player = varargin2struct( ...
+            opts.player, ...
+            'adaptive_mode',    'continuous', ... % 'continuous' adaptive playback
+            'record_mic',       true, ...   % record playback and vocal responses via recording device. 
+            'randomize',        false, ...   % randomize trial order before playback
+            'append_files',     true, ...  % append files before playback (makes one long trial)
+            'playback_channels',[1 2], ...  % channels to present sounds to. 
+            'window_fhandle',   @hann, ...  % windowing function handle (see 'window.m' for more options)
+            'window_dur',       0.005, ...  % window duration in seconds.
+            'unmod_playbackmode', [], ... % no unmodulated sound
+            'unmod_channels',   [], ... % no unmodulated sound
+            'unmod_leadtime',   [], ... % no unmodulated sound
+            'unmod_lagtime',    [], ... % no unmodulated sound
+            'unmod_playback',   {{}});  % no unmodulated sound
+            
+        % ============================
+        % Modification check (modcheck) configuration        
+        % ============================
+        opts.player.modcheck=struct(...
+            'fhandle',  @ANL_modcheck_keypress, ...     % check for specific key presses
+            'keys',     [KbName('left') KbName('right')], ...  % first key makes sounds louder, second makes sounds quieter
+            'map',      zeros(256,1));
+        
+        % Assign keys in map
+        opts.player.modcheck.map(opts.player.modcheck.keys)=1; 
+        
+        % ============================
+        % Modifier configuration        
+        %   We use two modifiers, but CWB can't recall why. Need to check
+        %   this. 
+        % ============================
+        opts.player.modifier{1}=struct( ...
+            'fhandle',  @modifier_dBscale, ... % use a decibel scale
+            'dBstep',   1, ...  % use constant 1 dB steps
+            'change_step', 1, ...   % always 1 dB
+            'channels', 2, ...  % apply modification to channel 2
+            'scale_mode',   'immediate'); % use immediate (no history) scaling
             
     otherwise
         
