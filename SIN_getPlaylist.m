76195c12e3c80a473d18a94fa3112e9332801603
cwbishop <chriswbishop@gmail.com>
2014-06-18 14:09:44 -0700

    Extended help and additional methods.

--- SIN_getPlaylist.m
+++ SIN_getPlaylist.m
@@ -1,4 +1,4 @@
-function SIN_getPlaylist(varargin)
+function [playlist, list_dir, wavfiles] = SIN_getPlaylist(opts, varargin)
 %% DESCRIPTION:
 %
 %   Function to return a playlist for a specific test. This is essentially
@@ -73,7 +73,7 @@ function SIN_getPlaylist(varargin)
 %           'any':  truly randomized list selection, with replacement upon
 %                   each call
 %
-%   'UsedListMat':  string, path to a mat-file containing a list of which
+%   'UsedList':     string, path to a mat-file containing a list of which
 %                   lists have been used. CWB imagines a different file for
 %                   each participant. A single file should be used for all
 %                   tests if the user wants to prevent list repeats in
@@ -99,22 +99,86 @@ function SIN_getPlaylist(varargin)
 %   University of Washington
 %   6/14
 
+error('not game ready'); 
+
 %% GET PARAMETER LIST, PLACE IN STRUCTURE
-d=varargin2struct(varargin{:});  
+%   Combine options structure and parameter list. 
+d=varargin2struct(opts, varargin{:});  
+
+%% INITIALIZE RETURN VARIABLES
+playlist = {}; 
 
 %% GET LIST INFORMATION FOR TEST
 %   - call SIN_stiminfo
 %   - if lists exist (list dir isn't empty), move forward. Otherwise, just
 %   return the wavfiles (e.g., with ANL)
+[list_dir, wavfiles]=SIN_stiminfo(opts); 
+
+%% RETURN WAVFILES
+%   We only need to do the following if we have lists to choose from.
+%   If we don't (e.g., with ANL), then just return the wavfiles variable.
+if isempty(list_dir) 
+    playlist = wavfiles; 
+    return
+end % if isempty(list_dir)
 
 %% REMOVE USED LISTS
 %
 %   - Load the UsedListMat file and check to see which lists have been used
 %   previously.
-%   - Those that have been used previously should be removed from the
-%   available list before moving on.
+%   - Whether or not the lists should be removed depends on the 'Repeats'
+%   setting, which is user defined. See help SIN_getPlaylist for more
+%   information. 
+if isfield(d, 'UsedListMat') && ~isempty(d.UsedList) && ~isequal(d.Repeats, 'any')
+    
+    % Which of these lists have been presented previously?
+    isused = SIN_UsedListInfo(d.UsedList, 'task', {{'isused'}}, 'lists', {list_dir});
+    
+    % Determine number of non-repeated lists
+    nlists = numel(find(logical(isused))); 
+    
+    % Check to make sure we have enough lists. If we don't we need to do
+    % one of two things:
+    %   - If d.Repeats is set to 'allbefore', then go ahead and select a
+    %   list that has already been run.
+    %       - Note: we want a built-in precaution so we select from lists
+    %       that have been used the least XXX. Perhaps a new method to
+    %       SIN_UsedListInfo?? XXX
+    if nlists < d.NLists && isequal(d.Repeats, 'allbefore')
+        
+        % Find the UsedLists with the minimum number of repeats; that is,
+        % the lists that have been used in the fewest tests. 
+        %   - Can get number of tests using 'ntests' method of
+        %   SIN_UsedListInfo
+        ntests = SIN_UsedListInfo(d.UsedList, 'task', {{'ntests'}}, 'lists', {list_dir}); 
+        
+        
+        
+    elseif nlists < d.NLists && isequal(d.Repeats, 'none')
+        error('Not enough unused lists to generate the playlist');         
+    end % if nlists < d.NLists
+    
+    % End with a logical mask of lists that we can use
+    
+elseif isequal(d.Repeats, 'any')
+    
+    % If the user does not place constraints on repeats (that is, all lists
+    % can be repeated at any time during testing), then return a logical
+    % mask allowing all list_dir to be used. 
+    listmask = true(numel(list_dir), 1);     
+end % if isfield(d, ...
 
-%% 
+%% SELECT THE NUMBER OF PLAYLIST NEEDED
+%   - This must be specified in the opts.specific.numberoflists field
+
 
 %% RANDOMIZE LIST SELECTION
-%   - Either concatenate stimuli across lists, then 
+%   - Follow one of several randomization schemes
+
+%% APPEND STIMULUS LIST TO USED LIST
+%   - If the user wants to, we can automatically append the selected lists
+%   to the "used lists" mat file. 
+%   - This should be handled by an independent function so we can call it
+%   in other functions as well ... maybe SIN_updateUsedList...something
+%   like that
+
