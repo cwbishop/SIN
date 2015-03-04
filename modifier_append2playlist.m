fd98aae18ffe5f5f8a10eee1da8aa71057fae574
Chris Bishop <chriswbishop@gmail.com>
2015-03-03 16:01:51 -0800

    Fixed bug with modifier_append2playlist.m that appended playlist to the Used List even before the stimuli are presented.

--- modifier_append2playlist.m
+++ modifier_append2playlist.m
@@ -53,7 +53,20 @@ function [Y, d] = modifier_append2playlist(X, mod_code, varargin)
 %
 % Development:
 %
-%   Parameters
+%   1) Playback files are being added on the last trial, but the last trial
+%   may satisfy the criteria we need. If the criteria are satisfied by the
+%   END of the trial, then we don't want to add stimuli. If they are NOT
+%   satisfied, then we need to add stimuli. But we won't know if the player
+%   is in an exit state (all stimuli satisfied) until the last trial is
+%   over. CWB recommends having multiple locations for modifiers to run. 
+%
+%       - Actually, the added playlists are NOT being added to the UsedList
+%       data structure. Whoops! Still a problem, just of a different
+%       nature.
+%
+%       - Maybe we can append the files to the playback list, gather the
+%       list information, then (perhaps) wait until the NEXT trial to see
+%       if we actually present the data.
 %
 % Christopher W Bishop
 %   University of Washington
@@ -87,21 +100,37 @@ Y=X;
 if ~d.player.modifier{modifier_num}.initialized
     
     % Set the initialization flag
-    d.player.modifier{modifier_num}.initialized=true;
+    d.player.modifier{modifier_num}.initialized = true;
+    
+    % Initialize an "stimuli_appended" flag that will be used below to
+    % track whether or not we've appended stimuli to the playback list.
+    % Safe to assume we aren't appending stimuli out of the gate. 
+    d.player.modifier{modifier_num}.stimuli_appended = false; 
+    
+    % Also track which trial number we appended stimuli during. We'll use
+    % this below to track whether or not we have progressed beyond the
+    % first stimulus of the appended playlist.
+    d.player.modifier{modifier_num}.append_at_trial = [];
     
     return
     
 end % if ~d.player.modifier{modifier_num}.initialized
 
-% Make options structure for playlist creation
+%% EXTEND PLAYBACK LIST
+%   The playback list will be extended here as a precaution, but we won't
+%   actually append the list to the "UsedList" data structure until after
+%   the first trial of the appended files have been presented. 
 if trial == numel(d.sandbox.playback_list);
+    
+    % Tell user we're adding stimuli
+    cprintf('blue', 'Adding stimuli to playback list and loading them.'); 
     
     opts = d; 
     opts.specific.genPlaylist = d.player.modifier{modifier_num};
     opts.specific.genPlaylist = rmfield(opts.specific.genPlaylist, 'fhandle');
     opts.specific.genPlaylist = rmfield(opts.specific.genPlaylist, 'initialized');
-
-    playlist = SIN_getPlaylist(opts); 
+    opts.specific.genPlaylist.Append2UsedList = false;    
+    [playlist, used_lists] = SIN_getPlaylist(opts); 
 
     % Append playlist to existing playlist
     d.sandbox.playback_list = {d.sandbox.playback_list{:} playlist{:}}';
@@ -120,5 +149,47 @@ if trial == numel(d.sandbox.playback_list);
         evalin('caller', 'clear stim2append'); 
 
     end % if d.player.preload 
-
+    
+    % Set a flag to let CWB know that files have been appended. Store this
+    % in the modifier.
+    d.player.modifier{modifier_num}.stimuli_appended = true; 
+    
+    % Also track which trial number we were at when we last appended
+    % stimuli to the playback list
+    d.player.modifier{modifier_num}.append_at_trial(end+1) = trial; 
+    
+    % Track the lists that were appended.
+    d.player.modifier{modifier_num}.used_lists = used_lists;
+    
+    % Also track the appended files
+    d.player.modifier{modifier_num}.appended_files = playlist; 
+    
 end % if ...
+
+%% IF FIRST FILE IN PLAYLIST IS PRESENTED
+%   If we are presenting the first stimulus in the appended stimulus train,
+%   then append the list to the subject's used list data structure. 
+if d.player.modifier{modifier_num}.stimuli_appended && (trial == d.player.modifier{modifier_num}.append_at_trial(end) + 1) 
+    
+    % If the player is in the 'exit' state, then we want to ...
+    %   1) Remove the appended files from the playback list (sort of
+    %   wasteful and potentially dangerous ... might be better to leave
+    %   them there)
+    %   2) Do NOT append the list to the used list data structure
+    if ~isequal(d.player.state, 'exit')
+    
+        % Construct a query to send to SIN_getPlaylist that will do the
+        % list tracking for us.        
+        opts = d; 
+        opts.specific.genPlaylist = d.player.modifier{modifier_num};
+        opts.specific.genPlaylist.lists = d.player.modifier{modifier_num}.used_lists; 
+        opts.specific.genPlaylist.files = d.player.modifier{modifier_num}.appended_files; 
+        opts.specific.genPlaylist.Append2UsedList = true; % hard code so we append the the used play list
+        
+        % Call getPlaylist
+        %   This should 
+        SIN_getPlaylist(opts);         
+        
+    end % if ~isequal(d.player.state, 'exit')
+
+end % 
